8abc8156c8c2e557840588ebe3fb2eaf398bb625
Alexander J. Fang <fanga@eos26.cis.gvsu.edu>
2022-01-27 19:21:38 -0500

    alex's updates v1

--- Practice.java
+++ Practice.java
@@ -1,8 +1,37 @@
 package gitLab;
 
 public class Practice {
-    //compare #'s
-    public static void main(String[] args) {
-            System.out.print("Yo. This is an assignment.");
-    }
+	//meant to count up chocolate bars to a certain number, does not work
+	public int makeChocolate(int small, int big, int goal) {
+		  int attempt = 0;
+		  int bigBar = 5;
+		  int smallBar = 1;
+		  int bigToUse = 0;
+		  int smallToUse = 0;
+		  
+		  //while (attempt != goal){
+		    attempt = 0;
+		    bigToUse = 0;
+		    smallToUse = 0;
+		    while(attempt + 5 < goal - 5){
+		      bigToUse++;
+		      attempt+=bigBar;
+		      if(attempt == goal - 5){
+		        bigToUse++;
+		        attempt=+5;
+		      }
+		    }
+		    
+		    while(attempt + 1 < goal - 1){
+		      smallToUse++;
+		      attempt+=smallBar;
+		      if(attempt == goal - 1){
+		        smallToUse++;
+		        attempt=+1;
+		      }
+		    }
+		  //}
+		  return smallToUse;
+		}
+
 }
